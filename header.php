edebedb97b168e856e6fb70aa0cb314d98448ed4
Jash Doshi <shirothegamer69@gmail.com>
2022-01-12 12:54:27 +0530

    initial commit

---
+++ header.php
@@ -0,0 +1,14 @@
+<html lang ="en">
+<title>Book turfs online</title>
+<link rel="stylesheet" href="css/index-style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<body>
+    <header>
+        <a href="index.php" class = "header-turfit">TURFIT</a>
+        <a href = 'register.php' class= 'header-register'>
+        <button data-bn-type = 'button' class='btn-register'> Register </button>
+        </a>
+    </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+include "header.php"; 
+?>
+
+<html>
+
+
+</html>
+
